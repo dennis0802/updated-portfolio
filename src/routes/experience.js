7ad420a375dacf06146f09c0dd34ae8c679cf7b5
Dennis <dennisdao2001@hotmail.com>
2024-09-12 15:28:13 -0400

    Update Projects and Experience

--- src/routes/experience.js
+++ src/routes/experience.js
@@ -102,8 +102,8 @@ const Experience = () => {
                                 <h6>May 2021-Jun 2021</h6>
                                 <ul style={{textAlign: "left"}}>
                                     <li>
-                                        Collaborated remotely in the <b>Digital Main Street (DMS) Future Proof</b> program with Agile teams of 8 to develop digital marketing strategies and tools for
-                                        700+ Southwestern Ontario small businesses by developing photos, logos, social media campaigns, and infographics using <b>Photoshop</b> and <b>Illustrator</b>
+                                        Collaborated remotely in the <b>Digital Main Street (DMS) Future Proof</b> program with Agile teams of <b>8</b> to develop digital marketing strategies and tools for
+                                        <b>700+</b> Southwestern Ontario small businesses by developing photos, logos, social media campaigns, and infographics using <b>Photoshop</b> and <b>Illustrator</b>
                                     </li>
                                     <li>
                                         Delivered designs and documents to clients regularly, revising them based on their feedback until clients were satisfied
